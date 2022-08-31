1df5f22364f6d6a04b3b2e4219073caa7c920491
Jesús Caro <103667880+jcaroamber@users.noreply.github.com>
2022-08-30 18:50:53 -0700

    animations

---
+++ Assets/Scripts/Generic/Animations/AnimationParameter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnimationParameter", menuName = "ScriptableObjects/AnimationParameters/AnimationParameter")]
+public class AnimationParameter : ScriptableObject
+{
+    public string ParamaterName;
+}
---
+++ Assets/Scripts/Generic/Animations/FloatParameter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatParameter", menuName = "ScriptableObjects/AnimationParameters/FloatParameter")]
+public class FloatParameter : AnimationParameter
+{
+    public float Value;
+}
